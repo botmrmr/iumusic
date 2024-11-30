408e7518132f966dd908343d169ab900fa8a0bb2
botmrmr <basharseed86@gmail.com>
2024-11-30 16:46:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 480))
 LOGGER_ID = int(getenv("LOGGER_ID","-1001756578463"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5145609515))
+OWNER_ID = int(getenv("OWNER_ID", 8057737935))
 
 ## Fill these variables if you re deploying on heroku.
 # Your heroku app name
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botkial/iumusic",
+    "https://github.com/botmrmr/iumusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 APK = 5140000000
@@ -41,9 +41,9 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-CHANNEL_NAME = getenv("CHANNEL_NAME", "𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐨𝐋🧚‍♀")
-CHANNEL_LINK = getenv("CHANNEL_LINK", "YMMYn")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/K55DD")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "تحديثات سورس مرمر 🧚🏻‍♀️")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/mrmr44t")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mrmr44t")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -82,18 +82,18 @@ confirmer = {}
 ANK = AMK + 9515
 
 
-START_IMG_URL = getenv("START_IMG_URL","https://te.legra.ph/file/e8bdc13568a49de93b071.jpg")
-PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/f995c36145125aa44bd37.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL = getenv("START_IMG_URL","https://forkgraph.zaid.pro/file/cM0IFUOfvyTW")
+PING_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+PLAYLIST_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+STATS_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+TELEGRAM_AUDIO_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+TELEGRAM_VIDEO_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+STREAM_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+SOUNCLOUD_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+YOUTUBE_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+SPOTIFY_ARTIST_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+SPOTIFY_ALBUM_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
+SPOTIFY_PLAYLIST_IMG_URL = "https://forkgraph.zaid.pro/file/cM0IFUOfvyTW"
 
 DAV = ANK
 def time_to_seconds(time):
