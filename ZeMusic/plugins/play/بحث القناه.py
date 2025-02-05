d900296625b0b467de187f2e91b745a3df266ed8
botmrmr <basharseed86@gmail.com>
2025-02-05 08:44:22 +0300

    Create بحث القناه.py

---
+++ ZeMusic/plugins/play/بحث القناه.py
@@ -0,0 +1,89 @@
+import os
+import re
+import config
+import aiohttp
+import aiofiles
+import yt_dlp
+from yt_dlp import YoutubeDL
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from youtube_search import YoutubeSearch
+from ZeMusic import app
+from ZeMusic.plugins.play.filters import command
+
+
+def remove_if_exists(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+lnk = f"https://t.me/{config.CHANNEL_LINK}"
+Nem = config.BOT_NAME + " ابحث"
+
+@app.on_message(command(["song", "/song", "بحث", Nem,"يوت"]) & filters.channel)
+async def song_downloader3(client, message: Message):
+    query = " ".join(message.command[1:])
+    m = await message.reply_text("<b>⇜ جـارِ البحث ..</b>")
+    
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        if not results:
+            await m.edit("- لم يتم العثـور على نتائج حاول مجددا")
+            return
+
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        title_clean = re.sub(r'[\\/*?:"<>|]', "", title)
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title_clean}.jpg"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(thumb_name, mode='wb')
+                    await f.write(await resp.read())
+                    await f.close()
+
+        duration = results[0]["duration"]
+
+    except Exception as e:
+        await m.edit("- لم يتم العثـور على نتائج حاول مجددا")
+        print(str(e))
+        return
+    
+    await m.edit("<b>جاري التحميل ♪</b>")
+
+    try:
+        audio_file = await YouTube.download(results[0]["id"], None)
+
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+
+        await message.reply_audio(
+            audio=audio_file,
+            caption=f"⟡ {app.mention}",
+            title=title,
+            performer=info_dict.get("uploader", "Unknown"),
+            thumb=thumb_name,
+            duration=dur,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk),
+                    ],
+                ]
+            ),
+        )
+        await m.delete()
+
+    except Exception as e:
+        await m.edit(f"error, wait for bot owner to fix\n\nError: {str(e)}")
+        print(e)
+
+    try:
+        remove_if_exists(audio_file)
+        remove_if_exists(thumb_name)
+    except Exception as e:
+        print(e)
