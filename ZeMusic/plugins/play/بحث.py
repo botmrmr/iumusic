d57fddf863a0bd7f7d4bea0e2766355ddcbb39d1
botmrmr <basharseed86@gmail.com>
2025-02-05 09:16:52 +0300

    Update بحث.py

--- ZeMusic/plugins/play/بحث.py
+++ ZeMusic/plugins/play/بحث.py
@@ -1,9 +1,10 @@
 import os
 import re
+import requests
 import config
 import aiohttp
 import aiofiles
-# from ZeMusic.platforms.Youtube import cookie_txt_file
+from ZeMusic.platforms.Youtube import cookie_txt_file
 from config import OWNER_ID
 import yt_dlp
 from yt_dlp import YoutubeDL
@@ -11,7 +12,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from youtube_search import YoutubeSearch
 
-from ZeMusic import app, YouTube 
+from ZeMusic import app
 from ZeMusic.plugins.play.filters import command
 from ZeMusic.utils.database import is_search_enabled1, enable_search1, disable_search1
 
@@ -32,24 +33,12 @@ async def song_downloader1(client, message: Message):
     
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
-        if not results:
-            await m.edit("- لم يتم العثـور على نتائج حاول مجددا")
-            return
-
         link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:40]
-        title_clean = re.sub(r'[\\/*?:"<>|]', "", title)  # تنظيف اسم الملف
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title_clean}.jpg"
-        
-        # تحميل الصورة المصغرة
-        async with aiohttp.ClientSession() as session:
-            async with session.get(thumbnail) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(thumb_name, mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
         duration = results[0]["duration"]
 
     except Exception as e:
@@ -59,57 +48,42 @@ async def song_downloader1(client, message: Message):
     
     await m.edit("<b>جاري التحميل ♪</b>")
     
-    # ydl_opts = {
-        # "format": "bestaudio[ext=m4a]",  # تحديد صيغة M4A
-        # "keepvideo": False,
-        # "geo_bypass": True,
-        # "outtmpl": f"{title_clean}.%(ext)s",  # استخدام اسم نظيف للملف
-        # "quiet": True,
-        # "cookiefile": cookie_txt_file(),
-    # }
-
     try:
-        audio_file = await YouTube.download(results[0]["id"], None)
-        # حساب مدة الأغنية
+        audio_file, tre= await YouTube.download(results[0]["id"], None)
+        rep = f"⟡ {app.mention}"
+        host = str(rep)
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-
-        # إرسال الصوت
+        
         await message.reply_audio(
             audio=audio_file,
-            caption=f"⟡ {app.mention}",
+            caption=rep,
             title=title,
-            performer=app.me.username,
+            performer=host,
             thumb=thumb_name,
-            duration=dur
+            duration=dur,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk),
+                    ],
+                ]
+            ),
         )
-        try:
-            await app.send_audio(
-                chat_id="@vbbbbnnnm",  # معرف القناة التي تريد الإرسال إليها 
-                audio=audio_file,
-                caption=f"⟡ {app.mention}",
-                title=title,
-                performer=app.me.username,
-                thumb=thumb_name,
-                duration=dur
-            )
-        except:
-            pass
         await m.delete()
 
     except Exception as e:
-        await m.edit(f"error, wait for bot owner to fix\n\nError: {str(e)}")
+        await m.edit("error, wait for bot owner to fix")
         print(e)
 
-    # حذف الملفات المؤقتة
     try:
         remove_if_exists(audio_file)
         remove_if_exists(thumb_name)
     except Exception as e:
         print(e)
-
+        
 
 @app.on_message(command(["تعطيل اليوتيوب بالخاص"]) & filters.user(OWNER_ID))
 async def disable_search_command1(client, message: Message):
